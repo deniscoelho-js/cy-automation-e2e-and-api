f462812e8519abcd341e14a7c54eeda2defd5f67
Denis Coelho <deniscoelho.dev@gmail.com>
2023-06-26 20:26:59 -0300

    create issue

---
+++ cypress/e2e/gui/createIssue.cy.js
@@ -0,0 +1,26 @@
+import { faker } from '@faker-js/faker'
+
+describe('Create Issue', () => {
+  const issue = {
+    title: `issue-${faker.datatype.uuid()}`,
+    description: faker.random.words(3),
+    project: {
+      name: `project-${faker.datatype.uuid()}`,
+      description: faker.random.words(5),
+    },
+  }
+
+  //pre condicao que o usuario esteja logado e que o projeto já exista
+  beforeEach(() => {
+    cy.login()
+    cy.gui_createProject(issue.project)
+  })
+
+  it('Sucessfully', () => {
+    cy.gui_createIssue(issue)
+
+    cy.get('.issue-details')
+      .should('contain', issue.title)
+      .and('contain', issue.description)
+  })
+})
--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -51,3 +51,11 @@ Cypress.Commands.add('gui_createProject', (project) => {
   cy.get('.qa-initianlize-with-readme-checkbox').check()
   cy.contains('Create project').check()
 })
+
+Cypress.Commands.add('gui_createIssue', (issue) => {
+  cy.visit(`/${Cypress.env('user_name')}/${issue.project.name}/issues/new`)
+
+  cy.get('.qa-issueable-form-title').type(issue.title)
+  cy.get('.qa-issueable-form-description').type(issue.description)
+  cy.contains('Submit issue').click()
+})
