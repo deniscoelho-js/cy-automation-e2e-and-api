83ea0c33944b43d98c2e3cef9192b4a1130b48a6
Denis Coelho <deniscoelho.dev@gmail.com>
2023-07-08 00:13:06 -0300

    atualizacao

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,10 @@ module.exports = defineConfig({
   viewportWidth: 1280,
   e2e: {
     baseUrl: 'http://localhost',
+    env: {
+      hideCredentials: true,
+      requestMode: true,
+    },
   },
   fixturesFolder: false,
   video: true,
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,3 @@
 import './gui_commands'
 import './api_commands'
+import 'cypress-plugin-api'
